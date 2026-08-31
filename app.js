340169b54fce8bdf24deac518aba039086bc5e17
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-08-31 12:45:41 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -18,11 +18,35 @@ app.set('view engine', 'ejs')
 
 // 4 Routing code
 app.post('/create-item', (req, res) => {
-	// TODO: code with db here
+	console.log('user intered /')
+	const new_reja = req.body.reja
+	db.collection('plans').insertOne(
+		{
+			reja: new_reja,
+		},
+		(err, data) => {
+			if (err) {
+				console.log(err)
+				res.end('something went wrong')
+			} else {
+				res.end('successfully added')
+			}
+		},
+	)
 })
 
 app.get('/', function (req, res) {
-	res.render('reja')
+	console.log('user intered /')
+	db.collection('plans')
+		.find({})
+		.toArray((err, data) => {
+			if (err) {
+				console.log(err)
+				res.end('something went wrong')
+			} else {
+				res.render('reja', { items: data })
+			}
+		})
 })
 
 module.exports = app
