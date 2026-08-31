7e6546ed05a82f31680592ba603b4d942626d20b
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-08-31 12:00:02 +0900

    feat: MongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,52 +1,37 @@
-// web serverni boshlash
-console.log('Server is running...')
-const express = require('express')
-const app = express()
 const http = require('http')
-const fs = require('fs')
+const mongodb = require('mongodb')
 
-let user
-fs.readFile('database/user.json', 'utf-8', function (err, data) {
-	if (err) {
-		console.log('ERROR:', err)
-		return
-	} else {
-		user = JSON.parse(data)
-	}
-})
+let db
+const connectionString =
+	'mongodb+srv://ismatbektaylonov_db_user:m0Lehgx4x4qxg71C@cluster0.zoflhhq.mongodb.net/?appName=Cluster0'
 
-//1: Kirish kodlari
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+mongodb.connect(
+	connectionString,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err, client) => {
+		if (err) console.log('ERROR on connection MongoDB')
+		else {
+			console.log('MongoDB connection succeed')
 
-//2: Session kodlari
+			db = client.db('Reja')
 
-//3: View kodlari
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+			module.exports = {
+				db: function () {
+					return db
+				},
+			}
 
-//4: Routing kodlari
-app.post('/create-item', function (req, res) {
-	console.log(req.body)
-	res.json({ name: 'Ismatbek' })
-})
-
-app.get('/author', function (req, res) {
-	res.render('author', {
-		user: user,
-	})
-})
-
-app.get('/', function (req, res) {
-	res.render('reja')
-})
-// app.get('/hello', function (req, res) {
-// 	res.end(`<h1 style="color: red;">Hello World! by Ismatbek</h1>`)
-// })
-
-const server = http.createServer(app)
-let PORT = 3000
-server.listen(PORT, function () {
-	console.log(`Server is running on http://localhost:${PORT}`)
-})
+			const app = require('./app')
+			const server = http.createServer(app)
+			let PORT = 3000
+			server.listen(PORT, function () {
+				console.log(
+					`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
+				)
+			})
+		}
+	},
+)
