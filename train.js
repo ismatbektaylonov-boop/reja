c13a673836fea285b45fab9004423c225ee393a1
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-09-02 15:09:22 +0900

    C-TASK: completed

--- train.js
+++ train.js
@@ -1,19 +1,87 @@
+// C-TASK:
+/*
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!*/
+
+const moment = require('moment')
+class Shop {
+	constructor(non, lagman, cola) {
+		this.non = non
+		this.lagman = lagman
+		this.cola = cola
+	}
+
+	qoldiq() {
+		const now = moment().format('HH:mm')
+		console.log(
+			`hozir ${now}da ${this.non}ta non, ${this.lagman}ta lag'mon va ${this.cola}ta cola mavjud`,
+		)
+	}
+
+	sotish(food, amount) {
+		if (food === 'non') {
+			if (this.non >= amount) {
+				this.non -= amount
+				this.qoldiq()
+			} else {
+				console.log('Non yetarli emas')
+			}
+		} else if (food === 'lagman') {
+			if (this.lagman >= amount) {
+				this.lagman -= amount
+				this.qoldiq()
+			} else {
+				console.log("Lag'mon yetarli emas")
+			}
+		} else if (food === 'cola') {
+			if (this.cola >= amount) {
+				this.cola -= amount
+				this.qoldiq()
+			} else {
+				console.log('Cola yetarli emas')
+			}
+		} else {
+			console.log('Bunday mahsulot mavjud emas')
+		}
+	}
+
+	qabul(food, amount) {
+		if (food === 'non') {
+			this.non += amount
+			this.qoldiq()
+		} else if (food === 'lagman') {
+			this.lagman += amount
+			this.qoldiq()
+		} else if (food === 'cola') {
+			this.cola += amount
+			this.qoldiq()
+		} else {
+			console.log('Bunday mahsulot sotib olinmaydi')
+		}
+	}
+}
+
+const shop = new Shop(4, 5, 2)
+shop.qoldiq()
+shop.sotish('non', 3)
+shop.qabul('cola', 4)
+
 // B-TASK:
 
 // Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 // MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 
-let count = 0
-function countDigits(word) {
-	for (let i = 0; i < word.length; i++) {
-		if (word[i] === String(Number(word[i]))) {
-			count++
-		}
-	}
-	return count
-}
+// let count = 0
+// function countDigits(word) {
+// 	for (let i = 0; i < word.length; i++) {
+// 		if (word[i] === String(Number(word[i]))) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
 
-console.log(countDigits('ad2a54y79wet0sfgb9'))
+// console.log(countDigits('ad2a54y79wet0sfgb9'))
 
 // A-TASK:
 
